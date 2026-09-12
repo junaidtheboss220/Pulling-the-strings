5b5082e0a0889893ab59a1aee6bd9be0d49c3e3f
Jibran Mohamed <junaidtheboss220@gmail.com>
2026-09-12 13:22:45 +0100

    slicing

---
+++ main.py
@@ -0,0 +1,32 @@
+first_name = "Jibran"
+last_name = "Mohamed"
+
+# print the first letter from the first name.
+
+print (first_name[0])
+
+# print the fisrts letter from the last name variable
+
+print (last_name[0])
+
+#  range slice([start:end])
+
+# print fist 2 letters from the first name
+
+print (first_name[0:2])
+
+# print first 3 letters from the from the last name
+
+print (last_name[0:3])
+
+# membership (in / not in)
+
+# check if f is in or not in
+
+print("f" in first_name)
+
+# check if j is present in first name
+
+print("J" in first_name)
+
+print("m" in last_name)
